4459abae6f6a085a1295fbc5ab8820d7ac60b9a5
Toan <chan.misha95@mail.ru>
2022-09-21 16:44:25 +0300

    - UI улучшения

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -23,6 +23,7 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.becomeFirstResponder()
         tableView.contentInset = UIEdgeInsets(top: 51, left: 0, bottom: 0, right: 0)
         var cellNib = UINib(
             nibName: TableView.CellIdentifiers.searchResultCell,
